cc72b8cf0fd76f67ac2fb4d1d6c7750f1e866c01
Calliste Skouras <cskouras@college.harvard.edu>
2023-12-27 17:41:17 -0500

    Initial Strava App

---
+++ README.md
@@ -0,0 +1,2 @@
+Inspiration and initial codebase from: 
+https://betterprogramming.pub/connecting-and-mapping-strava-data-with-python-and-django-41456b874f3b
---
+++ StravaMap/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from .views import *
+
+urlpatterns = [
+    path('', base_map, name='Base Map View'),
+    path('connected/', connected_map, name='Connect Map View'),
+    path('oauth/', include('social_django.urls', namespace='social')),
+    #path('social-auth/', include('social_django.urls', namespace='social')),
+]
+
